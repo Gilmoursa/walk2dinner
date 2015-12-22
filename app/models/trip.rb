af57bb09fdf98c7b220fedd447c63270b6e189c3
Gilmoursa <gilmoursa@gmail.com>
2015-12-22 16:36:29 -0500

    build associations for models

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,5 @@
 class Trip < ActiveRecord::Base
+    has_one :weather
+    has_one :restaurant
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+    has_many :trips
+    has_many :restaurants, through: :trips
+    has_many :weathers, through: :trips
 end

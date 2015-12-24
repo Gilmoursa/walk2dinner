e920764ac7894d8b51d42ea7a1f6999b1a56a5fe
Gilmoursa <gilmoursa@gmail.com>
2015-12-24 12:59:13 -0500

    organize routes file add sessions routes for login and logout and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  #resources
   resources :users
   resources :trips
   resources :weathers
   resources :restaurants
+  #welcome
+  root to: 'welcome#index'
+  #login
+  resources :sessions, only: [:create, :destroy]
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+  #signup
+  get '/signup', to: 'users#new', as: "signup"
 end

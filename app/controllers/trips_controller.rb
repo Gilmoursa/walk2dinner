1ee49cbd16f8b2c26cbf9c5a4bbfe5ffb95fb7b5
Gilmoursa <gilmoursa@gmail.com>
2015-12-26 17:37:08 -0500

    begin trip controller

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,26 @@
+class TripsController < ApplicationController
+    before_action :require_login, only: [:edit]
+
+    def new
+        @trip = Trip.new
+    end
+
+    def create
+        @trip = Trip.new(trip_params)
+        if @trip.save
+            log_in(@trip)
+            redirect_to @trip
+        else
+            flash[:alert] = "Sorry, your trip could not be created."
+            render 'new'
+        end
+    end
+
+    def show
+        @trip = trip.find_by_id(params[:id])
+        if @trip.nil?
+            flash[:notice] = "Trip was not found, please login or sign up."
+            redirect_to signup_path
+        end
+    end
+end

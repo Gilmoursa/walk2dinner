4f5cc7df77877c1dab50f73f184c87d1c1ed6087
Gilmoursa <gilmoursa@gmail.com>
2015-12-28 13:23:57 -0500

    create instance of restaurant object in restaurant model and yelp initializer for yelp gem to generate api call for a restaurent

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,15 +3,15 @@
 # Table name: restaurants
 #
 #  id         :integer          not null, primary key
-#  name       :string
-#  address    :string
-#  city       :string
-#  state      :string
-#  zip        :string
-#  phone      :string
-#  cuisine    :string
-#  image_url  :string
-#  rating_url :string
+#  -name       :string
+#  -address    :string
+#  -city       :string
+#  -state      :string
+#  -zip        :string
+#  -phone      :string
+#  -cuisine    :string
+#  -image_url  :string
+#  -rating     :string
 #  snippet    :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -19,4 +19,24 @@
 
 class Restaurant < ActiveRecord::Base
     belongs_to :trip
+
+    def self.search(zip_code)
+        restaurant = Yelp.client.search(zip_code, {term: 'food'}).businesses.sample(1)
+        name = restaurant.first.name
+        #address
+        address = restaurant.first.location.address.first
+        city = restaurant.first.location.city
+        state = restaurant.first.location.state_code
+        zip = restaurant.first.location.postal_code
+        phone = restaurant.first.phone
+        #more restaurant info
+        cuisine = restaurant.first.categories.flatten.first
+        url = restaurant.first.url
+        rating = restaurant.first.rating
+        image_url = restaurant.first.image_url
+        snippet = restaurant.first.snippet_text
+        #create
+        Restaurant.create(name: name, location: location, phone: phone, food: food, url: url, postal: postal_code, rating:rating, image: image)
+      end
+
 end
